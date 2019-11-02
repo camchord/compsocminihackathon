99f20a17e225ae20dd0611ec1c7eeb03d28d9262
Cameron Chisholm <cam.j.chisholm@gmail.com>
2019-11-02 19:56:26 +0000

    search working, added todo list to readme

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import string_generator as sg
+
+g = sg.generator()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    string = g.generate_string(5)
+    print(string)
+    return render_template("index.html", string=string)
